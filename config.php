c634862bd89e8a126e3424b06a2ea5662c054768
Kanishka Deogade <kanishka.deog56@gmail.com>
2026-03-06 12:25:33 +0530

    Update config.php

--- config.php
+++ config.php
@@ -1,7 +1,7 @@
 <?php
 // Database configuration
-$host = "sql102.infinityfree.com";
-$user = "if0_40506907";
+$host = "sql100.infinityfree.com";
+$user = "if0_41318733";
 $pass = "Mayuri56";   // your XAMPP / WAMP / MySQL password
-$dbname = "if0_40506907_task_manager";
+$dbname = "if0_41318733_calendar_task_manager";
 ?>
